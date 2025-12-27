4e5cc2b627f9e0c576a07ca2866e909805d0277d
ElenaLamskova <elena.katysheva23062000@gmail.com>
2025-12-27 18:05:39 +0500

    Checkstyle violations fixed#3

--- src/main/java/ru/mentee/power/devtools/progress/ProgressTracker.java
+++ src/main/java/ru/mentee/power/devtools/progress/ProgressTracker.java
@@ -15,7 +15,7 @@ public class ProgressTracker {
             index++;
         }
         int totalRemaining = totalTotal - totalCompleted;
-        return "пройдено " + totalCompleted + " из " + totalTotal + " уроков осталось " + totalRemaining;
+        return "пройдено " + totalCompleted + " из " + totalTotal + " уроков осталось " + totalRemaining + "уроков";
     }
 
     public static void main(String[] args) {
